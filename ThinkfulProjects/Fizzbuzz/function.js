91319e3f6df5cb0542ce5dc7f71adc74019d28d4
taragaide <taragaide@yahoo.com>
2015-09-04 08:12:36 -0400

    first commit

---
+++ ThinkfulProjects/Fizzbuzz/function.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){ 
+
+var i=0;
+while (true){
+	++i;
+
+
+if (number%3==0){
+	console.log("Fizz");}
+
+else {
+	console.log("number");
+}
+
+if (number%5==0){
+	console.log("buzz")
+}
+
+else{
+	console.log("number");
+}
+
+}
+
+
+for (var i=0; i<100; i++){
+	console.log(value + ' ');
+}
+
+	
+
+
+//if (number%3==0){
+//	console.log("Fizz");}
+
+//else {
+//	console.log("number");
+//}
+
+//if (number%5==0){
+//	console.log("buzz")
+//}
+
+//else{
+//	console.log("number");
+//}
+
+//});
